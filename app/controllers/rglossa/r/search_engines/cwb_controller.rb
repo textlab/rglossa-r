e3f9d87ffa631d4b312348b5f634273c2d999639
Anders Nøklestad <anders.noklestad@iln.uio.no>
2013-09-10 00:42:03 +0200

    Initial implementation of the query_freq action

--- app/controllers/rglossa/r/search_engines/cwb_controller.rb
+++ app/controllers/rglossa/r/search_engines/cwb_controller.rb
@@ -1,7 +1,29 @@
+require 'rserve'
+
 module Rglossa
   module R
     module SearchEngines
       class CwbController < ApplicationController
+
+        def query_freq
+          query = params[:query]
+          corpus = params[:corpus].upcase
+
+          conn = Rserve::Connection.new
+          conn.eval('library("rcqp")')
+          corp = conn.eval(%Q{corp <- corpus("#{corpus}")})
+          subcorp = conn.eval(%Q{subcorp <- subcorpus(corp, '#{query}')})
+
+          freqs = conn.eval('freqs <- cqp_flist(subcorp, "match", "word")')
+
+          pairs = []
+          freqs.attr.to_ruby[0].zip(freqs.to_ruby) do |a, f|
+            pairs << [a, f]
+          end
+
+          render json: {pairs: pairs, success: true}
+        end
+
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rglossa::Engine.routes.draw do
+
+  namespace :r do
+    namespace :search_engines do
+      match 'cwb/query_freq', to: 'cwb#query_freq'
+    end
+  end
 end
